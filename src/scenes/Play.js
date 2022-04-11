f6fc515fc33b52bcdb1de431d47dd63ef94dbd7a
Jose <joavelar@ucsc.edu>
2022-04-10 18:54:27 -0700

    The r key now restart the game after it ends

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -70,7 +70,7 @@ class Play extends Phaser.Scene {
 
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(10000, () => {
+        this.clock = this.time.delayedCall(60000, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER',
             scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart',
@@ -80,6 +80,10 @@ class Play extends Phaser.Scene {
     }
 
     update() {
+        // check key input for restart 
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
         this.starfield.tilePositionX -= 4;
         if(!this.gameOver){
             this.p1Rocket.update();
