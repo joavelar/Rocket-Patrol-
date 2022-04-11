f2c8c1257b4ac897002aefc2e931bfb58f59d319
Jose <joavelar@ucsc.edu>
2022-04-10 19:13:40 -0700

    added spaceship explosion sound

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -134,5 +134,8 @@ class Play extends Phaser.Scene {
         // score add and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+
+        // for sound
+        this.sound.play('sfx_explosion');
     }
 }
