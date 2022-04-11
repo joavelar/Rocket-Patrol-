00a0802e1450ec7cdd6ae803eb8e7814234a3336
Jose <joavelar@ucsc.edu>
2022-04-10 18:39:35 -0700

    rset after certain time works

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -65,6 +65,14 @@ class Play extends Phaser.Scene {
         }
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
 
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(10000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER',
+            scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart',
+            scoreConfig).setOrigin(0.5);
+        }, null, this);
     }
 
     update() {
